ee7b60f905609a12ef5bf493fb808596f0362fcd
marinas <marishk0@mail.ru>
2023-01-24 23:58:31 +0300

    9th step - add PrimeGame

---
+++ src/games/prime.js
@@ -0,0 +1,27 @@
+import generateRandomNumber from '../generateRandomNumber.js';
+import startGame from '../index.js';
+
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+    let dividers = [];
+    for (let i = 1; i <= num; i += 1) {
+        if (num % i === 0) {
+            dividers.push(i);
+        }
+    }
+    return dividers.length == 2;
+};
+
+const startRound = () => {
+    const num = generateRandomNumber();
+    const correctAnswer = isPrime(num) ? 'yes' : 'no';
+
+    return [num, correctAnswer];
+};
+
+const brainPrime = () => {
+    startGame(startRound, task);
+};
+
+export default brainPrime;
