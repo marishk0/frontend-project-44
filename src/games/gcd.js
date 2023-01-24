7cd215abfecc27387ef69bafba488b262d1bb3c1
marinas <marishk0@mail.ru>
2023-01-24 23:04:38 +0300

    7th step - add GCD

---
+++ src/games/gcd.js
@@ -0,0 +1,36 @@
+import generateRandomNumber from '../generateRandomNumber.js';
+import startGame from '../index.js';
+
+const task = 'Find the greatest common divisor of given numbers.';
+
+const findGCD = (a, b) => {
+    let minNum;
+    let gcd = 1;
+    if (a > b) {
+        minNum = b;
+    } else {
+        minNum = a;
+    }
+
+    for (let i = 2; i <= minNum; i += 1) {
+        if ((a % i === 0) && (b % i === 0)) {
+            gcd = i;
+        }
+    }
+    return String(gcd);
+};
+
+const startRound = () => {
+    const a = generateRandomNumber(0, 50);
+    const b = generateRandomNumber(0, 50);
+
+    const question = `${a} ${b}`;
+    const correctAnswer = findGCD(a, b);
+    return [question, correctAnswer];
+};
+
+const brainGCD = () => {
+    startGame(startRound, task);
+};
+
+export default brainGCD;
